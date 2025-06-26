9b4dcab8437156f22b1fd3a14631f7b0202ccfc2
sahil-itsolutions4india <sahil.itsolutions4india@gmail.com>
2025-06-26 15:26:59 +0530

    updated model

--- billing/models.py
+++ billing/models.py
@@ -76,6 +76,7 @@ class Product(models.Model):
     tax_rate = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
     opening_stock=models.IntegerField(default=0)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    expiry_date= models.DateField(null=True, blank=True)
     status = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
