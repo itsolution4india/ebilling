26dc4891d9e9f33b320e4c1efbf57b8cdc676839
sahil-itsolutions4india <sahil.itsolutions4india@gmail.com>
2025-07-04 12:06:20 +0530

    updated model

--- billing/models.py
+++ billing/models.py
@@ -140,9 +140,11 @@ class InvoiceItem(models.Model):
     product_name = models.CharField(max_length=255)
     product_description = models.TextField(blank=True)
     quantity = models.IntegerField(default=1)
+    tax=models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
     rate = models.DecimalField(max_digits=10, decimal_places=2)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
-    
+    discount = models.CharField(max_length=20, blank=True)
+
     created_at = models.DateTimeField(auto_now_add=True)
     
     def save(self, *args, **kwargs):
